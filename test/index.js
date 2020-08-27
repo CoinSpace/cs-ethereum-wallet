456bab26c75466f06f529bbcf21949c642a1b858
nikashitsa <vernik91@gmail.com>
2020-08-27 17:48:28 +0600

    Add new methods

--- test/index.js
+++ test/index.js
@@ -3,6 +3,8 @@
 var assert = require('assert');
 var Wallet = require('../');
 var fixtures = require('./wallet');
+// eslint-disable-next-line max-len
+var RANDOM_SEED = '2b48a48a752f6c49772bf97205660411cd2163fe6ce2de19537e9c94d3648c85c0d7f405660c20253115aaf1799b1c41cdd62b4cfbb6845bc9475495fc64b874';
 
 describe('Ethereum Wallet', function() {
   var readOnlyWallet;
@@ -13,6 +15,81 @@ describe('Ethereum Wallet', function() {
 
   it('should have more tests', function() {
     assert.equal('hi', 'hi');
+  });
+
+  describe('constructor', function() {
+    it('with seed', function() {
+      var wallet = new Wallet({
+        networkName: 'ethereum',
+        seed: RANDOM_SEED
+      });
+      assert.ok(wallet);
+      assert.equal(wallet.isLocked, false);
+    });
+
+    it('with publicKey', function() {
+      var wallet = new Wallet({
+        networkName: 'ethereum',
+        publicKey: readOnlyWallet.etherWallet.pubKey.toString('hex')
+      });
+      assert.equal(wallet.addressString, readOnlyWallet.addressString);
+      assert.equal(wallet.isLocked, true);
+      assert.ok(wallet);
+    });
+  });
+
+  describe('lock', function() {
+    it('works', function() {
+      var wallet = new Wallet({
+        networkName: 'ethereum',
+        seed: RANDOM_SEED
+      });
+      assert.equal(wallet.isLocked, false);
+      wallet.lock();
+      assert.equal(wallet.etherWallet._privKey, null);
+      assert.equal(wallet.isLocked, true);
+    });
+  });
+
+  describe('unlock', function() {
+    it('works', function() {
+      var wallet = new Wallet({
+        networkName: 'ethereum',
+        publicKey: readOnlyWallet.etherWallet.pubKey.toString('hex')
+      });
+      assert.equal(wallet.isLocked, true);
+      wallet.unlock(readOnlyWallet.etherWallet.privKey.toString('hex'));
+      assert.equal(wallet.etherWallet.privKey.toString('hex'), readOnlyWallet.etherWallet.privKey.toString('hex'));
+      assert.equal(wallet.isLocked, false);
+    });
+  });
+
+  describe('dumpKeys', function() {
+    it('works', function() {
+      var wallet = new Wallet({
+        networkName: 'ethereum',
+        seed: RANDOM_SEED
+      });
+      var keys = wallet.dumpKeys();
+      assert.ok(keys);
+      assert.ok(keys.private);
+      assert.ok(keys.public);
+    });
+
+    it('dumped keys are valid', function() {
+      var wallet = new Wallet({
+        networkName: 'ethereum',
+        seed: RANDOM_SEED
+      });
+      var keys = wallet.dumpKeys();
+      var secondWalet = new Wallet({
+        networkName: 'ethereum',
+        publicKey: keys.public
+      });
+      secondWalet.unlock(keys.private);
+      assert.equal(wallet.etherWallet.privKey.toString('hex'), secondWalet.etherWallet.privKey.toString('hex'));
+      assert.equal(wallet.addressString, secondWalet.addressString);
+    });
   });
 
   describe('serialization & deserialization', function() {
