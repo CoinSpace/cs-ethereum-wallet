eefd1bbbafb56a0c7106a4c9a5430501c102522f
nikashitsa <vernik91@gmail.com>
2021-09-10 16:27:02 +0200

    chore: minor refactor

--- test/index.js
+++ test/index.js
@@ -7,6 +7,9 @@ const fixtures = require('./wallet');
 const RANDOM_SEED = '2b48a48a752f6c49772bf97205660411cd2163fe6ce2de19537e9c94d3648c85c0d7f405660c20253115aaf1799b1c41cdd62b4cfbb6845bc9475495fc64b874';
 // eslint-disable-next-line max-len
 const RANDOM_SEED_PUB_KEY = '6337e0b448708659a757fbee3b0aa049dbc15a8f86ff6b5cabbc4b04895a43fadadf3ecc234414ce3083824b37976c63e70d0d75f0b5315712c429eadcc5cd6e';
+const crypto = {
+  platform: 'ethereum',
+};
 
 describe('Ethereum Wallet', () => {
   let readOnlyWallet;
@@ -22,9 +25,7 @@ describe('Ethereum Wallet', () => {
   describe('constructor', () => {
     it('with seed', () => {
       const wallet = new Wallet({
-        crypto: {
-          platform: 'ethereum',
-        },
+        crypto,
         seed: RANDOM_SEED,
       });
       assert.ok(wallet);
@@ -33,9 +34,7 @@ describe('Ethereum Wallet', () => {
 
     it('with publicKey', () => {
       const wallet = new Wallet({
-        crypto: {
-          platform: 'ethereum',
-        },
+        crypto,
         publicKey: readOnlyWallet.etherWallet.pubKey.toString('hex'),
       });
       assert.strictEqual(wallet.addressString, readOnlyWallet.addressString);
@@ -62,9 +61,7 @@ describe('Ethereum Wallet', () => {
   describe('unlock', () => {
     it('works', () => {
       const wallet = new Wallet({
-        crypto: {
-          platform: 'ethereum',
-        },
+        crypto,
         publicKey: RANDOM_SEED_PUB_KEY,
       });
       assert.strictEqual(wallet.isLocked, true);
@@ -77,9 +74,7 @@ describe('Ethereum Wallet', () => {
   describe('publicKey', () => {
     it('works', () => {
       const wallet = new Wallet({
-        crypto: {
-          platform: 'ethereum',
-        },
+        crypto,
         seed: RANDOM_SEED,
       });
       const publicKey = wallet.publicKey();
@@ -88,16 +83,12 @@ describe('Ethereum Wallet', () => {
 
     it('key is valid', () => {
       const wallet = new Wallet({
-        crypto: {
-          platform: 'ethereum',
-        },
+        crypto,
         seed: RANDOM_SEED,
       });
       const publicKey = wallet.publicKey();
       const secondWalet = new Wallet({
-        crypto: {
-          platform: 'ethereum',
-        },
+        crypto,
         publicKey,
       });
       secondWalet.unlock(RANDOM_SEED);
